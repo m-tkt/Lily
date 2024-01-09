003515cc8102724f6d7301b24ea36a748fac2f75
m-tkt <moe.tkt96@gmail.com>
2024-01-09 23:36:59 +0900

    チャット一覧カードデザイン修正

--- lib/src/components/chat_card.dart
+++ lib/src/components/chat_card.dart
@@ -7,80 +7,64 @@ class ChatCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        child: SizedBox(
-          height: 230,
-          child: Stack(
-            children: [
-              Positioned(
-                top: 25,
-                left: 20,
-                // elevationを付けるためMaterialウィジェットを使用
-                child: Material(
-                  elevation: 4,
-                  child: Container(
-                    height: 180,
-                    width: MediaQuery.of(context).size.width * 0.95,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(0.0),
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.green[100],
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0x80000000),
+            offset: Offset(0, 4),
+            blurRadius: 6,
+          )
+        ],
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          splashColor: Color(0x30f010f0),
+          onTap: () => print('tap!'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                  ),
+                  child: Image.network(
+                    'https://picsum.photos/200',
+                    width: 100,
+                    height: 100,
+                  )),
+              const SizedBox(
+                width: 10,
+              ),
+              const Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 10),
+                    Text(
+                      'Title',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff333333),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-              // 画像部分
-              Positioned(
-                top: 35,
-                left: 30,
-                child: Card(
-                  child: Container(
-                    height: 150,
-                    width: 150,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                          'https://source.unsplash.com/300x200/?beach',
-                        ),
-                        fit: BoxFit.fill,
+                    Text(
+                      'Test description!',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.normal,
+                        color: Color(0xff333333),
                       ),
-                      shape: BoxShape.circle,
-                      color: Colors.transparent,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 3,
                     ),
-                  ),
-                ),
-              ),
-              const Positioned(
-                top: 35,
-                left: 200,
-                child: SizedBox(
-                  height: 150,
-                  width: 900,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      // タイトル
-                      Text(
-                        'タイトル',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 22,
-                        ),
-                      ),
-                      Divider(color: Colors.black),
-                      // 説明文
-                      Text(
-                        'ここに説明文が入ります。ここに説明文が入ります。ここに説明文が入ります。',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                        ),
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 3,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+                  ]),
             ],
           ),
         ),
